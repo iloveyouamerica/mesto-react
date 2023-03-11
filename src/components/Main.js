1ba580c5420d64eebd62d34510677c8224189222
Alexander <iloveyouamerica@yandex.ru>
2023-03-11 13:57:44 +0300

    feat: настроена загрузка данных пользователя по api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,25 @@
-// компонент Main
+import React, { useEffect } from "react";
+import api from '../utils/Api';
 
+// компонент Main
 function Main(props) {
+
+  const [userName, setUserName] = React.useState('');
+  const [userDescription, setuserDescription] = React.useState('');
+  const [userAvatar, setuserAvatar] = React.useState('');
+
+  useEffect(() => {
+    api.getUserInfo()
+      .then((data) => {
+        //console.log(data);
+        setUserName(data.name);
+        setuserDescription(data.about);
+        setuserAvatar(data.avatar);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
   return (
     <main className="main">
@@ -11,17 +30,17 @@ function Main(props) {
           aria-label="Редактировать аватар"
           id="avatar-edit-btn"
           onClick={props.onEditAvatar}>
-          <img src="<!--<%=require('./images/profile-info/profile-photo.jpg')%>->" alt="Аватар пользователя" className="profile__image" />
+          <img src={userAvatar} alt="Аватар пользователя" className="profile__image" />
         </button>
         <div className="profile__text-wrapper">
-          <h1 className="profile__name">Жак-Ив Куст{/* <!--Жак-Ив Кусто--> */}</h1>
+          <h1 className="profile__name">{userName}{/* <!--Жак-Ив Кусто--> */}</h1>
           <button
             type="button"
             className="profile__button-edit button"
             aria-label="Редактировать профиль"
             id="edit-btn"
             onClick={props.onEditProfile}></button>
-          <p className="profile__about">Исследователь океана{/* <!--Исследователь океана--> */}</p>
+          <p className="profile__about">{userDescription}{/* <!--Исследователь океана--> */}</p>
         </div>
         <button 
           type="button"
